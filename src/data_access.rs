8d6d1ac65056a78906a3cce859f1a9a102963f4f
Sean Sweet <sweet.sean42@gmail.com>
2023-04-29 00:53:16 -0500

    added password encryption

--- src/data_access.rs
+++ src/data_access.rs
@@ -1,16 +1,29 @@
 use actix_web::web;
 use diesel::{prelude::*, r2d2::{Pool, ConnectionManager}};
+use argon2::{self, Config};
+use rand::Rng;
+
 use crate::models::user_model::*;
 use super::schema::users;
 
 type DB = web::Data<Pool<ConnectionManager<SqliteConnection>>>;
+
+fn hash(password: &[u8]) -> String {
+	let salt = rand::thread_rng().gen::<[u8; 32]>();
+	let config = Config::default();
+	argon2::hash_encoded(password, &salt, &config).unwrap()
+}
+
+fn verify(hash: &str, password: &[u8]) -> bool {
+	argon2::verify_encoded(hash, password).unwrap()
+}
 
 pub async fn create_user_handler(pool: DB, item: web::Json<UserNew>) -> Result<User, diesel::result::Error> {
 	let mut conn = pool.get().unwrap();
 	let new_user = UserNew {
 		name: item.name.clone(),
 		email: item.email.clone(),
-		password: item.password.clone(),
+		password: hash(&item.password.as_bytes()),
 	};
 
 	diesel::insert_into(users::table)
@@ -40,8 +53,8 @@ pub async fn update_user_handler(pool: DB, id: i32, item: web::Json<UserNew>) ->
 		.set((
 			users::name.eq(&item.name),
 			users::email.eq(&item.email),
-			users::password.eq(&item.password),
-		))
+			users::password.eq(hash(&item.password.as_bytes()))),
+		)
 		.execute(&mut conn)?;
 
 
